2737c230dc12de84b4022f2fc122ade22ebb7a71
xiaoyv_LV <1807865155@qq.com>
2023-01-28 16:26:51 +0800

    类型文件下载说明

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##网易云项目 react-typescript
+
+##pnpm i --save-dev @types/styled-components 下载styled-components类型声明文件
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-// pnpm i --save-dev @types/styled-components 需要下载类型声明文件
 const FooterWrapper = styled.div`
   display: flex;
 `
